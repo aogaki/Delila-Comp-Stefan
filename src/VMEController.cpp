2adfdd3315adb326155062785bb46aa40b0459fe
~Stefan <81765383+stefanniculae2208@users.noreply.github.com>
2022-08-17 10:40:39 +0300

    Delete src directory

--- src/VMEController.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/VMEController.hpp"
-
-
-//g++ -fPIC -shared -o VMEController.o VMEController.cpp
-
-VMEController::VMEController(){};
-VMEController::~VMEController(){};
--- src/VMEDevice.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/VMEDevice.hpp"
-
-
-
-//g++ -fPIC -shared -o VMEDevice.o VMEDevice.cpp VMEController.o
-
-VMEDevice::VMEDevice(){};
-VMEDevice::~VMEDevice(){};
